6445a05bc02a45a4861342df664509ebb1732acd
Saull Brandão <saull@outlook.com>
2022-11-09 08:32:11 -0300

    query params

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 import { CursosService } from './cursos.service';
 
 @Component({
@@ -6,12 +8,33 @@ import { CursosService } from './cursos.service';
   templateUrl: './cursos.component.html',
   styleUrls: ['./cursos.component.css'],
 })
-export class CursosComponent implements OnInit {
+export class CursosComponent implements OnInit, OnDestroy {
   cursos: any[] = [];
+  pagina: number = 1;
+  inscricao: Subscription = new Subscription();
 
-  constructor(private cursosService: CursosService) {}
+  constructor(
+    private cursosService: CursosService,
+    private route: ActivatedRoute,
+    private router: Router
+  ) {}
+
+  proximaPagina() {
+    this.router.navigate(['/cursos'], {
+      queryParams: {
+        pagina: ++this.pagina,
+      },
+    });
+  }
 
   ngOnInit(): void {
     this.cursos = this.cursosService.getCursos();
+    this.inscricao = this.route.queryParams.subscribe((queryParams: any) => {
+      this.pagina = queryParams['pagina'];
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.inscricao.unsubscribe();
   }
 }
